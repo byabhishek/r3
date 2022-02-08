fd82b1552e1643b4f7f9757a954134091da03935
byabhishek <abhishek@pi314.in>
2022-02-09 00:38:37 +0530

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get  'pages/home'
   get 'about',to: 'pages#about'
   resources :articles ,only:[:show, :index, :create, :new, :edit, :update, :destroy]
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
